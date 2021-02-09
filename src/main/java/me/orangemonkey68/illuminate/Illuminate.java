d032a011ef2af5a1a64d22e758a0024e0eb0690a
Clare <clare.thomason05@gmail.com>
2021-02-08 23:37:21 -0600

    Initial commit

---
+++ src/main/java/me/orangemonkey68/illuminate/Illuminate.java
@@ -0,0 +1,10 @@
+package me.orangemonkey68.illuminate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Illuminate implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/orangemonkey68/illuminate/client/IlluminateClient.java
@@ -0,0 +1,13 @@
+package me.orangemonkey68.illuminate.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class IlluminateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
